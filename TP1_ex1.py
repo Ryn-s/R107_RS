efab3c9c0461ea7044749718acfe7cf5435edd7e
Rayan Saoud <rayan.saoud@uha.fr>
2024-11-06 11:22:37 +0100

    Update

--- TP1_ex1.py
+++
@@ -1,8 +0,0 @@
-nom = "SAOUD"
-prenom = "Rayan"
-math = 10.5
-anglais = 14.0
-info = 18.0
-promotion = 2024
-moy = (math + anglais + info) / 3
-print(f"L'étudiant {prenom} {nom} de la promotion {promotion} a une moyenne de {moy:.1f}")
--- TP1_ex3.py
+++
@@ -1,5 +0,0 @@
-jour=int(input("Entrez le jour du mois (1 à 31):"))
-heure = int(input("Entrez l'heure(0 à 23)"))
-minute = int(input("Entrez les minutes(0 à 59)"))
-minutes_ecoulees= (jour-1)*24*60 + heure * 60 + minute
-print("Nombres de minutes écoulées depuis le début du mois:", minutes_ecoulees)
